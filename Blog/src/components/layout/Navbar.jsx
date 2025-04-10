d6321866d50349a9c4381b834aee0a299ee14897
MohamedKadi <gamesmohamed14@gmail.com>
2025-04-10 21:19:45 +0100

    Navbar a to Link for navigation

--- Blog/src/components/layout/Navbar.jsx
+++ Blog/src/components/layout/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className="flex flex-row justify-between mt-6 mx-10 text-blue-900">
       <div className="hover:text-blue-700 transition-colors">
-        <a href="#">BLOGGY</a>
+        <Link to="/">BLOGGY</Link>
       </div>
       <div>
         <ul className="flex flex-row justify-between gap-20 ">
